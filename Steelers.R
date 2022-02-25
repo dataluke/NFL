1a3e23b773848d4cf56de5c61ff0ccb3f2cc2288
Lukas Diex <diex.lukas@gmail.com>
2022-02-25 21:00:05 +0100

    QB and RB stats

--- Steelers.R
+++ Steelers.R
@@ -3,5 +3,27 @@ library(tidyverse)
 library(gt)
 library(ggimage)
 
+pbp <- load_pbp()
+pbp_rp <- pbp %>%
+  filter(rush == 1 | pass ==1) %>%
+  filter(!is.na(epa))
 
-    
+steelers_qb <- pbp_rp %>%
+  filter(posteam == "PIT") %>%
+  filter(!is.na(passer_player_name)) %>%
+  group_by(passer_player_name) %>%
+  summarize(passes = n(),
+            avg_epa = mean(epa)) %>%
+  arrange(-avg_epa)
+
+steelers_rb <- pbp_rp %>%
+  filter(posteam == "PIT") %>%
+  filter(!is.na(rusher_player_name)) %>%
+  group_by(rusher_player_name) %>%
+  summarize(rushes = n(),
+            yards = sum(rushing_yards, na.rm = T),
+            tds = sum(touchdown == 1 & td_team == posteam),
+  ) %>%
+  arrange(-yards) %>%
+  head(10)
+            
